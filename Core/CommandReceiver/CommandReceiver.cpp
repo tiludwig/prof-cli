2c33c0657a20834d453d7cfadbefcf2a4df7d899
Tim Ludwig <ludwig.tim@t-online.de>
2019-03-29 14:06:23 +0100

    Use communicator

--- Core/CommandReceiver/CommandReceiver.cpp
+++ Core/CommandReceiver/CommandReceiver.cpp
@@ -9,6 +9,12 @@
 #include <cstdio>
 #include <stdlib.h>
 
+void clearScreen()
+{
+	printf("\033[2J"); // clear screen
+	fflush(stdout);
+}
+
 CommandReceiver::CommandReceiver()
 {
 
@@ -27,29 +33,36 @@ bool CommandReceiver::process(char value)
 	{
 		//printf("Got packet [id=%d]\n", lastReceivedPacket.id);
 
-		auto ptrPayload = &lastReceivedPacket.payload[0];
 		if (lastReceivedPacket.id == 65)
 		{
-			lastReceivedPacket.payload.push_back('\0');
-			uint32_t value = static_cast<uint32_t>(atoi(ptrPayload));
+			int32_t* ptrPayload = (int32_t*)&lastReceivedPacket.payload[0];
+			uint32_t value = static_cast<uint32_t>(ptrPayload[0]);
 
 			float wcet = value / 72.0;
 			uint32_t corr_value = value - 1073;
 			float corr_wcet = corr_value / 72.0;
 
-			printf("Time: %d\n", value);//, ~%.2f us (%d, ~%.2f us)\n", value, wcet, corr_value, corr_wcet);
+			printf("\033[1;0H\033[K\033[1;0H");
+			printf("Time: %u\n", value); //, ~%.2f us (%d, ~%.2f us)\n", value, wcet, corr_value, corr_wcet);
 
-		}
-		else if(lastReceivedPacket.id == 10)
-		{
-			int32_t* state_vector = (int32_t*)&lastReceivedPacket.payload[0];
+			printf("\033[2;0H\033[K\033[2;0H");
+
 			printf("state = [ ");
-			for(int i = 0; i < 4; i++)
+			for (int i = 1; i < 5; i++)
 			{
-				printf("%d ", state_vector[i]);
+				printf("%d ", ptrPayload[i]);
 			}
 			printf("]\n");
+
 		}
+		else if (lastReceivedPacket.id == 11)
+		{
+			printf("\033[3;0H\033[K\033[3;0H");
+
+			lastReceivedPacket.payload.push_back('\0');
+			printf("Result: %s\n", &lastReceivedPacket.payload[0]);
+		}
+		printf("\033[4;0H\033[K\033[4;0H");
 		printf("dropped %u packets\n", protocol.badPackets);
 		return true;
 	}
--- prof-cli.cpp
+++ prof-cli.cpp
@@ -1,12 +1,25 @@
-
 #include <cstdio>
 #include "Components/ComLink/SerialLink.h"
 #include "Core/CommandReceiver/CommandReceiver.h"
 
+#include <chrono>
+#include <thread>
+
+#include <iostream>
+
+void waitForPacket(SerialLink& link, CommandReceiver& receiver)
+{
+	auto data = link.readData();
+	while (receiver.process(data) == false)
+	{
+		data = link.readData();
+	}
+}
+
 int main()
 {
 	SerialLink link;
-	if(link.initialize() == false)
+	if (link.initialize() == false)
 	{
 		printf("Error initializing serial port\n");
 		return 0;
@@ -14,10 +27,24 @@ int main()
 
 	CommandReceiver receiver;
 
-	while(true)
+	uint8_t msg[] = { '#', 10, 2, 0, '?', 3, 'i', 104 };
+	uint8_t msg_2[] = { '#', 20, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, (uint8_t) -28 };
+
+	int* acc_x = (int*) &msg_2[3];
+	int* acc_y = (int*) &msg_2[7];
+
+	while (true)
 	{
-		auto data = link.readData();
-		receiver.process(data);
+		std::cout << "\nEnter acc_x: ";
+		std::cin >> *acc_x;
+		std::cout << "\nEnter acc_y: ";
+		std::cin >> *acc_y;
+		link.writeData(msg_2, 13);
+		waitForPacket(link, receiver);
+
+		link.writeData(msg, 8);
+		waitForPacket(link, receiver);
+		std::cin.get();
 	}
 	printf("Exiting\n");
 }
