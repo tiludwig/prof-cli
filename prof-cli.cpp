306837fb63ea94a85341e70e0cde26a5793f63ec
Tim Ludwig <ludwig.tim@t-online.de>
2019-04-08 21:10:14 +0200

    Use factory and pathmanager

--- prof-cli.cpp
+++ prof-cli.cpp
@@ -20,6 +20,9 @@
 #include "Core/Configuration/Configuration.h"
 #include "Core/TestinputProvider/TestinputProvider.h"
 
+#include "Factories/ComLinkFactory.h"
+#include "Utility/PathManager.h"
+
 using namespace UI;
 
 volatile bool bRunning = true;
@@ -50,175 +53,145 @@ void printDist(FrequencyDistribution& freqStat)
 
 int main(int argc, char** argv)
 {
-	try{
-	char buf[PATH_MAX];
-	readlink("/proc/self/exe", buf, PATH_MAX);
-
-	printf("path is: %s\n", buf);
-	std::string basePath(buf);
-	auto index = basePath.find_last_of('/');
-	if (index == std::string::npos)
+	try
 	{
-		printf("Unable to get base path\n");
-		return 1;
-	}
-	basePath.erase(index);
+		PathManager path;
+		path.initialize();
 
-	printf("path is: %s\nindex is: %d\n", basePath.c_str(), index);
-
-#ifndef DEBUG
-	index = basePath.find_last_of('/');
-	if (index == std::string::npos)
-	{
-		printf("Unable to get base path\n");
-		return 1;
-	}
-	basePath.erase(index);
-#endif
-
-	printf("path is: %s\n", basePath.c_str());
-
-	Configuration cfg;
-	std::ifstream stream(basePath + "/bin/config.ini");
-	if (!stream.is_open())
-	{
-		printf("Unable to locate configuration file\n");
-		return 1;
-	}
-	stream >> cfg;
-	printf("config loaded\n");
-	std::string pluginPath(basePath);
-	pluginPath += "/plugins/";
-
-	int iterations = std::stoi(cfg["measurement.iterations"]);
-	int uiUpdateIterations = std::stoi(cfg["core.ui-update-freq"]);
-
-	printf("iter and ui set\n");
-
-	std::string taskname(cfg["measurement.sut-name"]);
-	std::string inputprovider(pluginPath);
-	inputprovider += cfg["measurement.input-provider"];
-
-	TestinputProvider provider = TestinputProvider::loadPlugin(inputprovider.c_str());
-	if (provider.initialize(pluginPath) == false)
-	{
-		printf("Failed to initialize plugin\n");
-		return -2;
-	}
-	FrequencyDistribution freqStats;
-
-	signal(SIGINT, sigfunc);
-
-	CommandLineInterface ui;
-	ui.initialize(iterations, uiUpdateIterations);
-
-	IComLink* link;
-	std::string driver = cfg["core.comdriver"];
-	if (driver == "serial")
-	{
-		link = new SerialLink();
-	}
-	else
-	{
-		printf("Unable to locate communication driver '%s'\n", driver.c_str());
-		return -2;
-	}
-
-	if (link->initialize(cfg["core.device"]) == false)
-	{
-		printf("Error initializing serial port\n");
-		return 0;
-	}
-
-	PacketCommunicator communicator(link);
-
-	char profRequestBuffer = 'P';
-	//int32_t simAccValues[3] = { 1, 20, -1 };
-
-	packet_t profilingRequest = { 10, 1, &profRequestBuffer };
-	packet_t profilingTarget = { 30, static_cast<uint16_t>(taskname.length() + 1), const_cast<char*>(taskname.c_str()) };
-
-	communicator.request(profilingTarget);
-	unsigned int counter = 0;
-	int uiIterations = 0;
-	MinMax minMaxStats;
-	Variance varStats;
-	Variance timeStats;
-
-	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-	for (int i = 0; i < iterations; i++)
-	{
-		auto dataset = provider.getNextDataset();
-		packet_t simulatedAccValues = { 20, static_cast<uint16_t>(dataset.size), dataset.data };
-		auto response = communicator.request(simulatedAccValues);
-
-		/* initialize random seed: */
-		srand(time(NULL));
-
-		/* generate delay number between 1 and 10: */
-		int delayTime = rand() % 5 + 1;
-		std::this_thread::sleep_for(std::chrono::milliseconds(delayTime));
-
-		start = std::chrono::steady_clock::now();
-		response = communicator.request(profilingRequest);
-
-		counter++;
-		if (response.id == 65)
+		Configuration cfg;
+		std::ifstream stream(path.getBinarypath() + "config.ini");
+		if (!stream.is_open())
 		{
-			uint32_t* payload = (uint32_t*) response.payload;
-			uint32_t execTime = payload[0];
+			printf("Unable to locate configuration file\n");
+			return 1;
+		}
+		stream >> cfg;
 
-			minMaxStats.update(execTime);
-			wcet = minMaxStats.getMax();
-			varStats.update(execTime);
-			freqStats.update(execTime);
+		int iterations = std::stoi(cfg["measurement.iterations"]);
+		int uiUpdateIterations = std::stoi(cfg["core.ui-update-freq"]);
 
-			if (uiIterations == uiUpdateIterations)
+		std::string taskname(cfg["measurement.sut-name"]);
+		std::string inputprovider(path.getPluginpath());
+		inputprovider += cfg["measurement.input-provider"];
+
+		TestinputProvider provider = TestinputProvider::loadPlugin(inputprovider.c_str());
+		if (provider.initialize(path.getPluginpath()) == false)
+		{
+			printf("Failed to initialize plugin\n");
+			return -2;
+		}
+		FrequencyDistribution freqStats;
+		signal(SIGINT, sigfunc);
+
+		CommandLineInterface ui;
+		ui.initialize(iterations, uiUpdateIterations);
+
+		auto link = ComLinkFactory::CreateComLink(cfg["core.comdriver"]);
+		if(link == nullptr)
+		{
+			printf("Failed to load communication driver.\n");
+			return -1;
+		}
+
+		if (link->initialize(cfg["core.device"]) == false)
+		{
+			printf("Error initializing serial port\n");
+			return 0;
+		}
+
+		PacketCommunicator communicator(link);
+
+		char profRequestBuffer = 'P';
+		//int32_t simAccValues[3] = { 1, 20, -1 };
+
+		packet_t profilingRequest = { 10, 1, &profRequestBuffer };
+		packet_t profilingTarget = { 30, static_cast<uint16_t>(taskname.length() + 1),
+				const_cast<char*>(taskname.c_str()) };
+
+		communicator.request(profilingTarget);
+		unsigned int counter = 0;
+		int uiIterations = 0;
+		MinMax minMaxStats;
+		Variance varStats;
+		Variance timeStats;
+
+		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+		for (int i = 0; i < iterations; i++)
+		{
+			auto dataset = provider.getNextDataset();
+			packet_t simulatedAccValues = { 20, static_cast<uint16_t>(dataset.size), dataset.data };
+			auto response = communicator.request(simulatedAccValues);
+
+
+
+			/* initialize random seed: */
+			srand(time(NULL));
+
+			/* generate delay number between 1 and 10: */
+			int delayTime = rand() % 5 + 1;
+			std::this_thread::sleep_for(std::chrono::milliseconds(delayTime));
+
+			start = std::chrono::steady_clock::now();
+			response = communicator.request(profilingRequest);
+
+			counter++;
+			if (response.id == 65)
 			{
-				uiIterations = 0;
-				ui.printResult(minMaxStats.getMax());
-				printf("\tMean: %f\n", varStats.getMean());
-				printf("\tStd.Dev.: %f\n", sqrt(varStats.getVariance()));
-				printf("\tSample Count: %u\n", varStats.getSampleCount());
-				//printDist(freqStats);
+				uint32_t* payload = (uint32_t*) response.payload;
+				uint32_t execTime = payload[0];
+
+				minMaxStats.update(execTime);
+				wcet = minMaxStats.getMax();
+				varStats.update(execTime);
+				freqStats.update(execTime);
+
+				if (uiIterations == uiUpdateIterations)
+				{
+					uiIterations = 0;
+					ui.printResult(minMaxStats.getMax());
+					printf("\tMean: %f\n", varStats.getMean());
+					printf("\tStd.Dev.: %f\n", sqrt(varStats.getVariance()));
+					printf("\tSample Count: %u\n", varStats.getSampleCount());
+					//printDist(freqStats);
+				}
+				uiIterations++;
+				ui.updateCurrentProgressbar(uiIterations);
+				ui.updateTotalProgressbar(i);
 			}
-			uiIterations++;
-			ui.updateCurrentProgressbar(uiIterations);
-			ui.updateTotalProgressbar(i);
+
+			end = std::chrono::steady_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+			timeStats.update(duration);
+			if ((i % 100) == 0)
+			{
+				double meanduration = timeStats.getMean();
+				int64_t estimatedSeconds = meanduration * (iterations - i) / 1000000;
+				ui.printRemainingTime(estimatedSeconds);
+			}
+
+			printf("Setting: %d %d %d\n", ((int32_t*)dataset.data)[0], ((int32_t*)dataset.data)[1], ((int32_t*)dataset.data)[2]);
 		}
 
-		end = std::chrono::steady_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		timeStats.update(duration);
-		if ((i % 100) == 0)
-		{
-			double meanduration = timeStats.getMean();
-			int64_t estimatedSeconds = meanduration * (iterations - i) / 1000000;
-			ui.printRemainingTime(estimatedSeconds);
-		}
-	}
+		fflush(stdout);
+		printf("\e[2J\e[0;0H");
+		fflush(stdout);
+		printf("Profiling done.\n");
+		printf("Measured WCET: %u cycles [%.2f us].\n", wcet, (wcet / 72.0));
 
-	fflush(stdout);
-	printf("\e[2J\e[0;0H");
-	fflush(stdout);
-	printf("Profiling done.\n");
-	printf("Measured WCET: %u cycles [%.2f us].\n", wcet, (wcet / 72.0));
+		auto dist = freqStats.getDistribution();
 
-	auto dist = freqStats.getDistribution();
+		std::sort(dist->begin(), dist->end(), [](freqdist_entry_t a, freqdist_entry_t b)
+		{	return a.value < b.value;});
 
-	std::sort(dist->begin(), dist->end(), [](freqdist_entry_t a, freqdist_entry_t b)
-	{	return a.value < b.value;});
+		std::ofstream myfile;
+		myfile.open("wcet-dist.csv");
+		for (auto& slot : *dist)
+			myfile << slot.value << "," << slot.counts << "\n";
 
-	std::ofstream myfile;
-	myfile.open("wcet-dist.csv");
-	for (auto& slot : *dist)
-		myfile << slot.value << "," << slot.counts << "\n";
-
-	myfile.close();
-	}
-	catch(const char* msg)
+		myfile.close();
+	} catch (const char* msg)
 	{
 		printf("Error: %s\n", msg);
 	}
