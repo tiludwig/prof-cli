28c5b758bc110b6c63303c1432279277b319ccf6
Tim Ludwig <ludwig.tim@t-online.de>
2019-04-08 18:11:25 +0200

    Add configuration module

---
+++ Core/Configuration/Configuration.cpp
@@ -0,0 +1,92 @@
+/*
+ * Configuration.cpp
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: tim
+ */
+
+#include <Core/Configuration/Configuration.h>
+
+#include <algorithm>
+
+using namespace std;
+Configuration::Configuration()
+{
+}
+
+Configuration::~Configuration()
+{
+}
+
+std::string& Configuration::getValue(const std::string& key)
+{
+	auto value = find_if(config.begin(), config.end(), [&](const std::pair<std::string, std::string>& element)
+	{
+		return element.first == key;
+	});
+
+	if (value != config.end())
+	{
+		return value->second;
+	}
+	else
+	{
+		throw "Unable to find key in configuration";
+	}
+}
+
+void Configuration::add(const std::string& key, const std::string& value)
+{
+	auto setting = make_pair(key, value);
+	config.push_back(setting);
+}
+
+std::string& Configuration::operator[](const std::string& key)
+{
+	return getValue(key);
+}
+
+std::string ltrim(const std::string& text)
+{
+	auto firstChar = text.find_first_not_of(" \t");
+	return text.substr(firstChar, text.size() - firstChar);
+}
+
+std::string rtrim(const std::string& text)
+{
+	auto lastChar = text.find_last_not_of(" \t");
+	return text.substr(0, lastChar+1);
+}
+
+std::string trim(const std::string& text)
+{
+	return rtrim(ltrim(text));
+}
+
+void operator>>(std::istream& stream, Configuration& cfg)
+{
+	string line;
+	string currentSection = "";
+	while (getline(stream, line))
+	{
+		// remove comments
+		auto commentIndex = line.find('#');
+		if(commentIndex != string::npos)
+			line.erase(commentIndex);
+
+		auto tokenIndex = line.find('[');
+		if(tokenIndex != string::npos)
+		{
+			auto endIndex = line.find(']');
+			currentSection = trim(line.substr(tokenIndex+1, endIndex - tokenIndex - 1));
+			continue;
+		}
+		auto separatorIndex = line.find('=');
+		if(separatorIndex == string::npos)
+			continue;
+
+		auto key = trim(line.substr(0, separatorIndex));
+		auto value = trim(line.substr(separatorIndex +1, line.size() - separatorIndex));
+		cfg.add(currentSection + '.' + key, value);
+	}
+}
---
+++ Core/Configuration/Configuration.h
@@ -0,0 +1,33 @@
+/*
+ * Configuration.h
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: tim
+ */
+
+#ifndef CORE_CONFIGURATION_CONFIGURATION_H_
+#define CORE_CONFIGURATION_CONFIGURATION_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Configuration
+{
+private:
+	std::vector<std::pair<std::string, std::string>> config;
+public:
+	Configuration();
+	~Configuration();
+
+	void loadFromFile(const std::string& filename);
+
+	std::string& getValue(const std::string& key);
+	std::string& operator[](const std::string& key);
+
+	void add(const std::string& key, const std::string& value);
+
+	friend void operator>>(std::istream&, Configuration&);
+};
+
+#endif /* CORE_CONFIGURATION_CONFIGURATION_H_ */
