830905ad304647bd3f09b7332175c5917e314d2c
Tim Ludwig <ludwig.tim@t-online.de>
2019-05-22 13:14:49 +0200

    Remove unecessary function from configuration

--- Core/Configuration/Configuration.h
+++ Core/Configuration/Configuration.h
@@ -12,6 +12,8 @@
 #include <string>
 #include <vector>
 
+
+
 class Configuration
 {
 private:
@@ -28,12 +30,6 @@ public:
 	void add(const std::string& key, const std::string& value);
 
 	friend void operator>>(std::istream&, Configuration&);
-
-	unsigned int getIterations();
-	unsigned int getUiUpdateFrequency();
-	std::string& getTaskName();
-	std::string& getInputProviderName();
-	std::string& getComdriverType();
 
 	bool getValueIfExists(const std::string& key, std::string& value);
 };
